4e8589079663dd1a490a5f32f75bd78ef99166b5
ivasilinets <ivasilinets@gridgain.com>
2015-06-05 16:35:15 +0300

    #nodejs: add test classes.

--- modules/nodejs/src/test/java/org/apache/ignite/internal/NodeJsSelfTest.java
+++ modules/nodejs/src/test/java/org/apache/ignite/internal/NodeJsSelfTest.java
@@ -1,20 +1,24 @@
 /*
- *  Copyright (C) GridGain Systems. All Rights Reserved.
- *  _________        _____ __________________        _____
- *  __  ____/___________(_)______  /__  ____/______ ____(_)_______
- *  _  / __  __  ___/__  / _  __  / _  / __  _  __ `/__  / __  __ \
- *  / /_/ /  _  /    _  /  / /_/ /  / /_/ /  / /_/ / _  /  _  / / /
- *  \____/   /_/     /_/   \_,__/   \____/   \__,_/  /_/   /_/ /_/
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package org.apache.ignite.internal;
 
-import org.apache.ignite.cache.*;
-import org.apache.ignite.configuration.*;
 import org.apache.ignite.internal.util.*;
 import org.apache.ignite.internal.util.typedef.*;
-import org.apache.ignite.internal.util.typedef.internal.*;
-import org.apache.ignite.testframework.junits.common.*;
 
 import java.util.*;
 import java.util.concurrent.*;
@@ -24,36 +28,7 @@ import static java.util.concurrent.TimeUnit.*;
 /**
  * Test node js client.
  */
-public class NodeJsSelfTest extends GridCommonAbstractTest {
-    /** Cache name. */
-    private static final String CACHE_NAME = "mycache";
-
-    /** {@inheritDoc} */
-    @Override protected IgniteConfiguration getConfiguration(String gridName) throws Exception {
-        IgniteConfiguration cfg = super.getConfiguration(gridName);
-        cfg.setCacheConfiguration(cacheConfiguration());
-
-        ConnectorConfiguration conCfg = new ConnectorConfiguration();
-
-        conCfg.setJettyPath(getNodeJsTestDir() + "rest-jetty.xml");
-
-        cfg.setConnectorConfiguration(conCfg);
-
-        return cfg;
-    }
-
-    /**
-     * @return Cache configuration.
-     */
-    private CacheConfiguration cacheConfiguration() {
-        CacheConfiguration ccfg = new CacheConfiguration();
-
-        ccfg.setName(CACHE_NAME);
-        ccfg.setAtomicityMode(CacheAtomicityMode.ATOMIC);
-
-        return ccfg;
-    }
-
+public class NodeJsSelfTest extends NodeJsAbstractTest {
     /**
      * @throws Exception If failed.
      */
@@ -100,19 +75,5 @@ public class NodeJsSelfTest extends GridCommonAbstractTest {
             if (proc != null)
                 proc.killProcess();
         }
-    }
-
-    /**
-     * @return Node js test dir.
-     */
-    private String getNodeJsTestDir() {
-        String sep = System.getProperty("file.separator");
-
-        return U.getIgniteHome() +
-            sep + "modules" +
-            sep + "nodejs" +
-            sep + "src" +
-            sep + "test" +
-            sep + "nodejs" + sep;
     }
 }
