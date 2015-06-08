2c7426bf24ad7d0935933eab8d7641b9cf7c69ec
ivasilinets <ivasilinets@gridgain.com>
2015-06-08 13:01:34 +0300

    #nodejs: rename directories nodejs to js.

--- modules/nodejs/src/test/js/test_ignition.js
+++ modules/nodejs/src/test/js/test_ignition.js
@@ -1,7 +1,24 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 var TestUtils = require("./test_utils").TestUtils;
 var Ignition = require(TestUtils.scriptPath() + "ignition").Ignition;
 
-exports.test_ignition_fail = function ()  {
+testIgnitionFail = function ()  {
     Ignition.start(['127.0.0.3:9091', '127.0.0.1:9092'], onConnect);
 
     function onConnect(error, server) {
@@ -18,8 +35,8 @@ exports.test_ignition_fail = function ()  {
     }
 }
 
-exports.ignition_start_success = function() {
-    Ignition.start(['127.0.0.1:9095'], onConnect);
+ignitionStartSuccess = function() {
+    Ignition.start(['127.0.0.0:9090', '127.0.0.1:9090'], onConnect);
 
     function onConnect(error, server) {
         if (error) {
