a989aa09da1719359325d639f47296fae8b7d716
Denis Magda <dmagda@gridgain.com>
2015-06-10 14:27:11 +0300

    ignite-471-2: fixing .net

--- modules/core/src/main/java/org/apache/ignite/internal/interop/InteropIgnition.java
+++ modules/core/src/main/java/org/apache/ignite/internal/interop/InteropIgnition.java
@@ -48,11 +48,13 @@ public class InteropIgnition {
      * @return Ignite instance.
      */
     public static synchronized InteropProcessor start(@Nullable String springCfgPath, @Nullable String gridName,
-        int factoryId, long envPtr) {
+                                                      int factoryId, long envPtr) {
         IgniteConfiguration cfg = configuration(springCfgPath);
 
         if (gridName != null)
             cfg.setGridName(gridName);
+        else
+            gridName = cfg.getGridName();
 
         InteropBootstrap bootstrap = bootstrap(factoryId);
 
